0600ab8b0f5491d4617c25a2d698e9a1af889b30
einalem4 <melanie@codehangar.io>
2021-03-04 18:41:43 -0700

    add html routes

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(express.json());
 //file path to the public folder and instruct the server to make these files static resources.
 app.use(express.static('public'));
 
+// returns the notes.html file
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//returns the index.html file
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
